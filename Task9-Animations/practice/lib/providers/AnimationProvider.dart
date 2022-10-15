6abb95180aed3a1ac6534b840e77da65f7083eb0
Hunia <nadeem.h9696@gmail.com>
2022-10-15 23:24:35 +0500

    init

---
+++ Task9-Animations/practice/lib/providers/AnimationProvider.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+
+class AnimationProvider with ChangeNotifier {
+  late double squareSize;
+  late int colorIntensity;
+  double x = 0;
+  double y = 0;
+  late double radius;
+
+  int randomIntensity() {
+    var intensity = Random().nextInt(9) * 100;
+    return intensity == 0 ? 50 : intensity;
+  }
+
+  double randomSquare() {
+    var size = Random().nextInt(300);
+    return size < 10 ? 30 : size.toDouble();
+  }
+
+  double randomDimension() {
+    var dimension = Random().nextInt(3);
+    return dimension == 2 || x == 1 || y == 1 ? -1 : dimension.toDouble();
+  }
+
+  double randomRadius() {
+    var radius = Random().nextInt(100);
+    return radius < 10 ? 10 : radius.toDouble();
+  }
+
+  void changeState() {
+    squareSize = randomSquare();
+    x = randomDimension();
+    y = randomDimension();
+    colorIntensity = randomIntensity();
+    radius = randomRadius();
+    notifyListeners();
+  }
+}
---
+++ Task9-Animations/practice/lib/screens/AnimationWidget.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:practice/providers/animationProvider.dart';
+import 'package:provider/provider.dart';
+
+class AnimationWidget extends StatefulWidget {
+  const AnimationWidget({super.key});
+
+  @override
+  State<AnimationWidget> createState() => _AnimationWidgetState();
+}
+
+class _AnimationWidgetState extends State<AnimationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AnimationProvider>(
+      builder: ((context, provider, child) {
+        return GestureDetector(
+          onTap: Provider.of<AnimationProvider>(context, listen: false).changeState(),
+          child: Scaffold(
+            backgroundColor: Colors.grey,
+            appBar: AppBar(
+              title: const Text("Tap the Screen!"),
+              backgroundColor: Colors.black,
+              centerTitle: true,
+            ),
+            body: SafeArea(
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                alignment: Alignment(provider.x, provider.y),
+                curve: Curves.easeIn,
+                child: Container(
+                  width: provider.squareSize,
+                  height: provider.squareSize,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(provider.radius),
+                    color: Colors.red[provider.colorIntensity],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
